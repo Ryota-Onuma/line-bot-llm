53e329d94c36d670ff3301224801daf880f3de34
Ryota-Onuma <onumaryotajh.tech@gmail.com>
2025-04-29 01:37:24 +0900

    feat: add routing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
-
 plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.ktor)
+    kotlin("plugin.serialization") version "1.9.22"
 }
 
 group = "com.example"
@@ -26,6 +26,8 @@ dependencies {
     implementation(libs.ktor.server.netty)
     implementation(libs.logback.classic)
     implementation(libs.ktor.server.config.yaml)
+    implementation("io.ktor:ktor-server-content-negotiation:2.3.7")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.7")
     testImplementation(libs.ktor.server.test.host)
     testImplementation(libs.kotlin.test.junit)
 }
--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -8,17 +8,27 @@ import io.ktor.server.plugins.openapi.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import com.example.model.*
 
 fun Application.configureRouting() {
     install(DoubleReceive)
     routing {
-        post("/double-receive") {
-            val first = call.receiveText()
-            val theSame = call.receiveText()
-            call.respondText(first + " " + theSame)
-        }
         get("/") {
             call.respondText("Hello World!")
+        }
+        get("/tasks") {
+            println("/tasks called")
+            call.respond(
+                listOf(
+                    Task("cleaning", "Clean the house", Priority.Low),
+                    Task("gardening", "Mow the lawn", Priority.Medium),
+                    Task("shopping", "Buy the groceries", Priority.High),
+                    Task("painting", "Paint the fence", Priority.Medium)
+                )
+            )
+        }
+        post("/line") {
+             println("/line called")
         }
     }
 }
