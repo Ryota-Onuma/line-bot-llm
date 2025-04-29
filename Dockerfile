b730ea526a91aa674e4d576a79d864d2eaf27cf3
Ryota-Onuma <onumaryotajh.tech@gmail.com>
2025-04-29 10:27:01 +0900

    Dockerfile修正

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,25 @@
-FROM eclipse-temurin:23.0.2_7-jre-ubi9-minimal
-EXPOSE 8080
-RUN mkdir /app
-WORKDIR /app/
+FROM eclipse-temurin:23.0.2_7-jdk-ubi9-minimal AS builder
+WORKDIR /app
+
+# Install xargs
+RUN microdnf install -y findutils
+
+COPY gradle/wrapper/ gradle/wrapper/
+COPY gradlew gradlew.bat build.gradle.kts gradle.properties settings.gradle.kts ./ 
+
+RUN chmod +x gradlew \
+    && ./gradlew --no-daemon dependencies
+
 COPY . .
-CMD ["./gradlew", "run"]
+
+RUN ./gradlew --no-daemon clean assemble
+
+
+FROM eclipse-temurin:23.0.2_7-jre-ubi9-minimal
+WORKDIR /app
+
+COPY --from=builder /app/build/libs/*.jar ./app.jar
+
+EXPOSE 8080
+
+ENTRYPOINT ["java", "-jar", "app.jar"]
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,24 @@
-rootProject.name = "ktor-sample"
+rootProject.name = "line-bot-llm"
+
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+            version("kotlin", "1.9.22")
+            version("ktor", "2.3.7")
+            
+            plugin("kotlin.jvm", "org.jetbrains.kotlin.jvm").versionRef("kotlin")
+            plugin("ktor", "io.ktor.plugin").versionRef("ktor")
+            
+            library("ktor.server.core", "io.ktor", "ktor-server-core").versionRef("ktor")
+            library("ktor.server.netty", "io.ktor", "ktor-server-netty").versionRef("ktor")
+            library("ktor.server.double.receive", "io.ktor", "ktor-server-double-receive").versionRef("ktor")
+            library("ktor.line.webhook.plugin", "io.ktor", "ktor-server-line-webhook").versionRef("ktor")
+            library("ktor.server.openapi", "io.ktor", "ktor-server-openapi").versionRef("ktor")
+            library("ktor.server.config.yaml", "io.ktor", "ktor-server-config-yaml").versionRef("ktor")
+            library("ktor.server.test.host", "io.ktor", "ktor-server-test-host").versionRef("ktor")
+            
+            library("logback.classic", "ch.qos.logback", "logback-classic").version("1.4.11")
+            library("kotlin.test.junit", "org.jetbrains.kotlin", "kotlin-test-junit").versionRef("kotlin")
+        }
+    }
+}
